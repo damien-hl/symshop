9a1ecb5fdba0af46e39381435b50456888728d90
Damien Heulin <damienheulin87@gmail.com>
2021-04-09 16:27:04 +0200

    Ajout du composant de mail

--- src/EventDispatcher/ProductViewEmailSubscriber.php
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -4,7 +4,10 @@ namespace App\EventDispatcher;
 
 use App\Event\ProductViewEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 /**
  * Class ProductViewEmailSubscriber
@@ -15,11 +18,22 @@ class ProductViewEmailSubscriber implements EventSubscriberInterface
     /**
      * @var LoggerInterface
      */
-    private LoggerInterface $logger;
+    protected LoggerInterface $logger;
 
-    public function __construct(LoggerInterface $logger)
+    /**
+     * @var MailerInterface
+     */
+    protected MailerInterface $mailer;
+
+    /**
+     * ProductViewEmailSubscriber constructor.
+     * @param LoggerInterface $logger
+     * @param MailerInterface $mailer
+     */
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     /**
@@ -37,6 +51,18 @@ class ProductViewEmailSubscriber implements EventSubscriberInterface
      */
     public function sendProductEmail(ProductViewEvent $productViewEvent)
     {
+//        $email = new TemplatedEmail();
+//        $email->from(new Address('contact@mail.com', 'Infos de la boutique'))
+//            ->to('admin@mail.com')
+//            ->text('Un visiteur est en train de voir la page du produit ' . $productViewEvent->getProduct()->getName())
+//            ->htmlTemplate('emails/product_view.html.twig')
+//            ->context([
+//                'product' => $productViewEvent->getProduct()
+//            ])
+//            ->subject('Visite du produit ' . $productViewEvent->getProduct()->getName());
+//
+//        $this->mailer->send($email);
+
         $this->logger->info("Email envoyé pour le produit " . $productViewEvent->getProduct()->getName());
     }
 }
--- src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
+++ src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
@@ -2,9 +2,15 @@
 
 namespace App\EventDispatcher;
 
+use App\Entity\User;
 use App\Event\PurchaseSuccessEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * Class PurchaseSuccessEmailSubscriber
@@ -15,15 +21,29 @@ class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
     /**
      * @var LoggerInterface
      */
-    private  LoggerInterface $logger;
+    protected LoggerInterface $logger;
+
+    /**
+     * @var MailerInterface
+     */
+    protected MailerInterface $mailer;
+
+    /**
+     * @var Security
+     */
+    protected Security $security;
 
     /**
      * PurchaseSuccessEmailSubscriber constructor.
      * @param LoggerInterface $logger
+     * @param Security $security
+     * @param MailerInterface $mailer
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, Security $security, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->security = $security;
     }
 
     /**
@@ -38,9 +58,27 @@ class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
 
     /**
      * @param PurchaseSuccessEvent $purchaseSuccessEvent
+     * @throws TransportExceptionInterface
      */
     public function sendSuccessEmail(PurchaseSuccessEvent $purchaseSuccessEvent)
     {
+        /** @var User $currentUser */
+        $currentUser = $this->security->getUser();
+
+        $purchase = $purchaseSuccessEvent->getPurchase();
+
+        $email = new TemplatedEmail();
+        $email->to(new Address($currentUser->getEmail(), $currentUser->getFullName()))
+            ->from("contact@mail.com")
+            ->subject("Bravo votre commande ({$purchase->getId()}) a bien été confirmée")
+            ->htmlTemplate('emails/purchase_success.html.twig')
+            ->context([
+                'purchase' => $purchase,
+                'user' => $currentUser
+            ]);
+
         $this->logger->info("Email envoyé pour la commande n°" . $purchaseSuccessEvent->getPurchase()->getId());
+
+        $this->mailer->send($email);
     }
 }
